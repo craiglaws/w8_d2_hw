e17d354c8747c104ecb6329b41803f1a2def6e65
craiglaws <craiglaws1@gmail.com>
2020-06-23 16:47:45 +0100

    HW complete with extentions

---
+++ client/src/components/SightingsForm.vue
@@ -0,0 +1,72 @@
+<template lang="html">
+	<form id="sightings-form" v-on:submit="addSighting" method="post">
+		<h2>Add a Sighting</h2>
+		<div class="formWrap">
+			<label for="species">Species:</label>
+			<input type="text" v-model="species" id="species" />
+		</div>
+		<div class="formWrap">
+			<label for="location">Location:</label>
+			<input type="text" v-model="location" id="location" />
+		</div>
+		<div class="formWrap">
+			<label for="date">Date:</label>
+			<input type="date" v-model="date" id="date" />
+		</div>
+
+		<input type="submit" value="Save" id="save"/>
+	</form>
+</template>
+
+<script>
+import SightingService from '../services/SightingService.js'
+import {eventBus} from '../main.js'
+export default {
+	name: 'sightings-form',
+	data() {
+		return {
+			species: "",
+			location: "",
+			date: null
+		}
+	},
+	methods: {
+		addSighting(e){
+			e.preventDefault()
+			const sighting = {
+				species: this.species,
+				location: this.location,
+				date: this.date
+			}
+			SightingService.postSighting(sighting)
+			.then(res => eventBus.$emit('sighting-added', res));
+			this.species = this.location = "";
+			this.date = null
+		}
+	}
+}
+</script>
+
+<style lang="css" scoped>
+h2 {
+	margin: 10px 0;
+	padding: 0;
+}
+
+form {
+	width: 75%;
+	margin: 0 auto;
+	background: rgba(255, 255, 255, 0.7);
+	padding: 20px;
+	margin-bottom: 40px;
+}
+
+label {
+	min-width: 100px;
+	display: inline-block;
+}
+
+.formWrap {
+	margin-bottom: 10px;
+}
+</style>
